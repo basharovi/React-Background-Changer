7e44fb5d5384533cdbf677997366cba2a3047fee
Mominur Bashar Ovi <bashar.ovi@outlook.com>
2024-02-10 21:31:35 +0600

    Added the Code For transition

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,28 @@
 import React, { useState } from 'react'
 
 function App() {
-  const [color, setColor] = useState('lightblue');
+  const [color, setColor] = useState('grey');
 
   return (
-    <div className="w-full h-screen duration-500"
+    <div className="w-full h-screen transition-all duration-1000 ease-in flex justify-center items-center"
     style={{backgroundColor: color}}
     >
+
+    <div className="fixed right-5 flex flex-col justify-center inset-y-0 gap-3">
+     
+      <button className="bg-red-400 px-3 py-1 rounded-lg shadow-md"
+              onClick={() => setColor("darkred") } >Red</button>
+
+      <button className="bg-blue-400 px-3 py-1 rounded-lg"
+              onClick={() => setColor("lightblue") }>Blue</button>
       
+      <button className="bg-green-400 px-3 py-1 rounded-lg"
+              onClick={() => setColor("green") }>Green</button>
     </div>
+
+    </div>
+
+    
   )
 }
 
