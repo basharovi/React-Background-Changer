2cf7c2767bcfeac57408ebaedbff9cecf6ab5ab9
Bashar Ovi <bovi@cantaloupe.com>
2025-02-20 19:56:19 +0600

    Added more colour button

--- src/App.jsx
+++ src/App.jsx
@@ -8,16 +8,29 @@ function App() {
     style={{backgroundColor: color}}
     >
 
-    <div className="fixed right-5 flex flex-col justify-center inset-y-0 gap-3">
+    <div className="fixed left-5 flex flex-col justify-center gap-4">
      
-      <button className="bg-red-400 px-3 py-1 rounded-lg shadow-md"
+      <button className="bg-red-500 px-3 py-1 rounded-lg shadow-md"
               onClick={() => setColor("darkred") } >Red</button>
 
-      <button className="bg-blue-400 px-3 py-1 rounded-lg"
-              onClick={() => setColor("lightblue") }>Blue</button>
+      <button className="bg-blue-400 px-3 py-1 rounded-lg shadow-md"
+              onClick={() => setColor("darkblue") }>Blue</button>
       
-      <button className="bg-green-400 px-3 py-1 rounded-lg"
+      <button className="bg-green-400 px-3 py-1 rounded-lg shadow-md"
               onClick={() => setColor("green") }>Green</button>
+    </div>
+
+    <div className="fixed right-5 flex flex-col justify-center gap-4">
+     
+
+    <button className="bg-teal-400 px-3 py-1 rounded-lg shadow-md"
+              onClick={() => setColor("teal") }>Teal</button>
+
+      <button className="bg-yellow-500 px-3 py-1 rounded-lg shadow-md"
+              onClick={() => setColor("yellow") } >Yellow</button>
+
+      <button className="bg-cyan-400 px-3 py-1 rounded-lg shadow-md"
+              onClick={() => setColor("cyan") }>Cyan</button>
     </div>
 
     </div>
